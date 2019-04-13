bcf12b93d72e9658a1b383efd177dc9b78a859d3
Gabriel Debes <gabriel.wady@gmail.com>
2019-04-13 16:55:42 -0700

    Sample program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include "./SerialLib/src/serialLib/SerialController.h"
+#include "./ComponentsLib/src/Potentiometer.h"
+
+static SerialController *serialCom;
+static Potentiometer *throttle;
+#define PIN1 20
+#define PIN2 21
 
 void setup() {
-  // put your setup code here, to run once:
+  serialCom = new SerialController();
+  pinMode(PIN1, OUTPUT);
+  pinMode(PIN2, OUTPUT);
+  throttle = new Potentiometer(A2,0,1000);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  serialCom->loop();
+  if (GetControlStatus(serialCom->getVesselData(), RCS) == 1) {
+    digitalWrite(PIN1, HIGH);
+  }else {
+    digitalWrite(PIN1, LOW);
+  }
+  if (GetControlStatus(serialCom->getVesselData(), SAS) == 1) {
+    digitalWrite(PIN2, HIGH);
+  }else {
+    digitalWrite(PIN2, LOW);
+  }
+  throttle->loop();
+  SerializedVesselControls *control = new SerializedVesselControls();
+  control->Throttle = throttle->getCurrentValue();
+  serialCom->setVesselControls(*control);
 }
